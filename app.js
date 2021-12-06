e517e2d1bb9cc16fc85cdbe6143b1b6f35bcdb75
benjaminPla <benjaminpla.dev@gmail.com>
2021-12-06 10:17:04 -0300

    add express routes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 import express from 'express';
+import router from './routes/router.js';
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ ok: true, msg: 'all good!' });
-});
+app.use(router);
 
 app.listen('3001', () => console.log('API on!'));
